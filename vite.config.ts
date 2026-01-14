1d30a38ea56f740341b039d129e3ee4ad5f2e682
EliasGit117 <95446990+EliasGit117@users.noreply.github.com>
2026-01-14 17:54:01 +0200

    Refactored `vite.config.ts` to streamline plugins, update alias resolution, and adjust `paraglideVitePlugin` configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,29 @@
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { resolve } from 'node:path'
 import { paraglideVitePlugin } from '@inlang/paraglide-js'
-import { defineConfig } from 'vite';
-import { devtools } from '@tanstack/devtools-vite';
-import viteReact from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr';
 
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    devtools(),
+    tailwindcss(),
     paraglideVitePlugin({
-      cookieName: 'lang',
       project: './project.inlang',
       outdir: './src/paraglide',
-      strategy: ['cookie', 'localStorage', 'preferredLanguage', 'baseLocale']
+      outputStructure: 'message-modules',
+      cookieName: 'locale',
+      strategy: ['cookie', 'localStorage', 'preferredLanguage', 'baseLocale'],
     }),
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    tanstackRouter({ autoCodeSplitting: true }),
     viteReact({ babel: { plugins: ['babel-plugin-react-compiler'] } }),
-    tailwindcss(),
     svgr()
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-});
+      '@': resolve(__dirname, './src'),
+    },
+  },
+})
